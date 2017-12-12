cf81c40ab729a5f9f328dc37bdd06efff7b12b19
Benjamin Huang <bhuang@rms.com>
2017-12-11 22:33:29 -0800

    refactoring

---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+export default class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
