f14a9b28dc1d03870aaa4ce733b1a206d934792c
Benjamin Huang <bhuang@rms.com>
2017-12-12 21:18:29 -0800

    update

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 
-export default class Square extends React.Component {
-    /*
-        React components can have state by setting this.state in the constructor, 
-        which should be considered private to the component.
-    */
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-    render() {
-        return (
-            //Whenever this.setState is called, an update to the component is scheduled
-            <button className="square" onClick={() => this.setState({value: 'X'})}>
-                 {this.state.value}
-            </button>
-        );
-    }
+// class Square extends React.Component {
+//     /*
+//         React components can have state by setting this.state in the constructor, 
+//         which should be considered private to the component.
+//     */
+//     constructor(props) {
+//         super(props);
+//         // this.state = {
+//         //     value: null,
+//         // };
+//     }
+//     render() {
+//         return (
+//             //Whenever this.setState is called, an update to the component is scheduled
+//             // <button className="square" onClick={() => this.setState({value: 'X'})}>
+//             //      {this.state.value}
+//             // </button>
+//             <button className="square" onClick={() => this.props.onClick()}>
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
+
+
+export default function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
